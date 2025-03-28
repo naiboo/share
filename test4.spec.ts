e0da450ea1803e6412343b3535c2a662267fa5ff
naiboo <naiboo@gmx.at>
2025-03-28 07:37:23 +0100

    Add files via upload

---
+++ test4.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import { Counter, Gauge } from 'playwright-prometheus-remote-write-reporter';
+
+const URLvar='https://playwright.dev/';
+
+const gotoCounter = new Counter({
+    name: 'url',
+  }, 0)
+  
+test.beforeEach(async ({ page }) => {
+    await page.goto(URLvar);
+    //test.setTimeout(60000);
+    gotoCounter.labels({
+      url: URLvar
+    }).inc() 
+});
+
+
+test.describe('Test 4', () => {
+test('Test 4', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.waitForTimeout(10000); // hard wait for 5000ms
+  });
+});
+
+
+test.afterEach(() => {
+  gotoCounter.collect() // send to prometheus
+  gotoCounter.reset() // reset counter after each test
+})
